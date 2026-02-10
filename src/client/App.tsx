6f2c4c210071ca4dc2de5a1f93c955848e640db1
john <johnmarchoeksema@gmail.com>
2026-02-10 14:44:45 -0500

    client-server communication works

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,36 +1,37 @@
 import "./App.css";
-
 import { useState } from "react";
 
-import reactLogo from "./assets/react.svg";
-
 function App() {
-  const [count, setCount] = useState(0);
+  const [input, setInput] = useState("")
+  const [chatLog, setChatLog] = useState([])
+
+  function handleInputChange(event: React.ChangeEvent<HTMLTextAreaElement>) {
+    setInput(event.target.value)
+  }
+
+  async function handleClick(event: React.MouseEvent<HTMLButtonElement>) {
+    //event.preventDefault()
+    const response = await fetch("http://localhost:3000/chat", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ userInput: input })}
+    )
+    const data = await response.json()
+    console.log("SERVER RESPONSE: ", data)
+  }
 
   return (
-    <div className="App">
+    <>
+      <h1>johns chatbot o7</h1>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <textarea 
+          value={input}
+          onChange={handleInputChange}
+        ></textarea>
+        <button onClick={handleClick}>Send</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  );
+    </>
+  )
 }
 
 export default App;
