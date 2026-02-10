aacea13d535f3b8581660cebadd43eeb08bc4319
john <johnmarchoeksema@gmail.com>
2026-02-10 16:10:39 -0500

    frontend chat history works! terrifying and ugly but it works

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,33 +1,51 @@
 import "./App.css";
 import { useState } from "react";
+import Anthropic from "@anthropic-ai/sdk";
+import ReactMarkdown from "react-markdown"
+
+const chatLog: Anthropic.MessageParam[] = []
 
 function App() {
   const [input, setInput] = useState("")
-  const [chatLog, setChatLog] = useState([])
+  const [chatLog, setChatLog] = useState<Anthropic.MessageParam[]>([])
 
   function handleInputChange(event: React.ChangeEvent<HTMLTextAreaElement>) {
     setInput(event.target.value)
   }
 
   async function handleClick(event: React.MouseEvent<HTMLButtonElement>) {
-    //event.preventDefault()
+    // isolate and save user message
+    const userMessage: Anthropic.MessageParam = { "role": "user", "content": input }
+    setChatLog(prev => prev.concat(userMessage))
+
+    // send post request
     const response = await fetch("http://localhost:3000/chat", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ userInput: input })}
+      body: JSON.stringify(userMessage)}
     )
-    const data = await response.json()
-    console.log("SERVER RESPONSE: ", data)
+
+    // isolate and save claude response 
+    const claudeResponse: Anthropic.MessageParam = await response.json()
+    setChatLog(prev => prev.concat(claudeResponse))
+    setInput("")
+
+    console.log("CHATLOG: ", chatLog)
   }
 
   return (
     <>
-      <h1>johns chatbot o7</h1>
+      <h1>bingobot</h1>
       <div>
+        <div className="text-window">
+          {chatLog.map((message) => (
+            <ReactMarkdown>{message.content as string}</ReactMarkdown>
+          ))}
+        </div>
         <textarea 
           value={input}
-          onChange={handleInputChange}
-        ></textarea>
+          onChange={handleInputChange}>
+        </textarea>
         <button onClick={handleClick}>Send</button>
       </div>
     </>
--- src/server/main.ts
+++ src/server/main.ts
@@ -7,50 +7,47 @@ const app = express();
 const client = new Anthropic();
 app.use(express.json())
 
-/*
-async function getClaudeResponse() {
-  const message = await client.messages.create({
-    model: "claude-opus-4-6",
-    max_tokens: 1024,
-    messages: [
-      {
-        role: "user",
-        content: "my name is john! how do i clean dishes better? I keep breaking them accidentally."
-      }
-    ]
-  })
-  return message.content
-}
-*/
-
-/*
-app.get("/hello", (_, res) => {
-  //res.send("Hello Vite + React + TypeScript!");
-  callAnthropic()
-});
-*/
+const chatLog: Anthropic.MessageParam[] = []
 
 app.post("/chat", async (req, res) => {
   try {
-    console.log("REQ BODY: ", req.body)
-    const userInput = req.body.userInput
-    console.log(userInput)
+    // isolate and save the user input
+    const userInputObj: Anthropic.MessageParam = req.body
+    chatLog.push(userInputObj)
+
+    // get claude response
     const response = await client.messages.create({
       model: "claude-haiku-4-5-20251001",
       max_tokens: 1024,
-      messages: [
-        {
-          role: "user",
-          content: userInput
-        }
-      ]
+      messages: chatLog
     })
-    res.send(response.content)
+
+    // isolate and save claude response
+    if (!response.content[0] || response.content[0].type != 'text') {
+      throw new Error('expected text response from Claude')
+    }
+    const claudeResponseObj: Anthropic.MessageParam = { "role": "assistant", "content": response.content[0].text }
+    chatLog.push( claudeResponseObj )
+
+    // send claude response
+    res.send( claudeResponseObj )
+
   } catch (err) {
     console.log(err)
     res.status(500).json({ error: "POST /chat endpoint broke" })
   }
 })
+
+/*
+app.post("/reset", (_, res) => {
+  try {
+
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({ error: "POST /reset endpoint broke" })
+  }
+})
+*/
 
 ViteExpress.listen(app, 3000, () =>
   console.log("Server is listening on port 3000..."),
